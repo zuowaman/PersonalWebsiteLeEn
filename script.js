b5da947429a0df283645025e9796b7f955b8f168
李恩 <evan9003@gmail.com>
2024-12-23 09:55:52 +0800

    導覽列RWD

--- script.js
+++ script.js
@@ -8,3 +8,12 @@ var $grid = $('.grid').imagesLoaded(function () {
     itemSelector: '.grid-item'
   });
 });
+
+window.addEventListener('scroll', function () {
+  const navbar = document.querySelector('.navbar');
+  if (window.scrollY > 50) {
+      navbar.classList.add('scrolled');
+  } else {
+      navbar.classList.remove('scrolled');
+  }
+});
