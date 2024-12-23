ac11ed96a7d59a25954e2ae9a1769cfe206892fb
LeeEn <evan9003@gmail.com>
2024-12-23 08:49:54 +0800

    重作了一遍

--- script.js
+++ script.js
@@ -1,28 +1,5 @@
-//Get the button
-let mybutton = document.getElementById("btn-back-to-top");
-
-console.log(mybutton);
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-  scrollFunction();
-};
-
-function scrollFunction() {
-  console.log('scroll');
-  if (
-    document.body.scrollTop > 20 ||
-    document.documentElement.scrollTop > 20
-  ) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-// When the user clicks on the button, scroll to the top of the document
-mybutton.addEventListener("click", backToTop);
-
-function backToTop() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
+// jQuery
+$('.grid').masonry({
+  columnWidth: 200,
+  itemSelector: '.grid-item'
+});
